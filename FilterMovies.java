2b9134dd57af0129c198bae2f8b95653bb93183d
christiasoul <csaario@gmail.com>
2017-04-11 18:47:29 -0700

    Testing Changes

--- FilterMovies.java
+++ FilterMovies.java
@@ -1,6 +1,10 @@
 package movies;
 
 public class FilterMovies{
-    public enum filterType{Name, MaleActor, FemaleActor, }
+    public enum filterType{Name, MaleActor, FemaleActor, Genre}
+    public enum filterComparison{Has, Is, Not}
+
+
+    
 }
 
